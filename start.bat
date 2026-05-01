4fee00752725deb23f2acc3d7fff88a5f2d7b737
volus1986 <volus1986@gmail.com>
2026-05-01 20:45:21 +0300

    docs: add instructions for configuring automatic startup via Task Scheduler in start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,11 @@
+@REM ============================================================
+@REM  To add this script to autorun on Windows OS:
+@REM  1. Press Win+R and enter: taskschd.msc
+@REM  2. Create a new task with administrator permissions
+@REM  3. Set the trigger to "At startup" or "At log on"
+@REM  4. Set the action to run this .bat file
+@REM  Don't forget to use administrator permissions!
+@REM ============================================================
+
 node "%~dp0dist\index.js"
 pause
